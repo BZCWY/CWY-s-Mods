51f161f84842956bc8bd3be55226e5beedba9fda
BZCWY <chunwuyu0@gmail.com>
2026-03-11 17:18:50 +0800

    updata

--- example_mods/Brain-rot/data/Brain-rot/Fuck.lua
+++ example_mods/Brain-rot/data/Brain-rot/Fuck.lua
@@ -11,47 +11,18 @@ function onCreatePost()
     if getProperty('healthBar') ~= nil then
         setProperty('healthBar.visible', false)
     end
-    if getProperty('healthBarBG') ~= nil then
-        setProperty('healthBarBG.visible', false)
-    end
-    if getProperty('iconP1') ~= nil then
-        setProperty('iconP1.visible', false)
-    end
-    if getProperty('iconP2') ~= nil then
-        setProperty('iconP2.visible', false)
-    end
-    if getProperty('scoreTxt') ~= nil then
-        setProperty('scoreTxt.visible', false)
-    end
-    if getProperty('missesTxt') ~= nil then
-        setProperty('missesTxt.visible', false)
-    end
-    if getProperty('rating') ~= nil then
-        setProperty('rating.visible', false)
-    end
-    if getProperty('ratingFC') ~= nil then
-        setProperty('ratingFC.visible', false)
-    end
-    if getProperty('botplayTxt') ~= nil then
-        setProperty('botplayTxt.visible', false)
-    end
-    if getProperty('timeBarBG') ~= nil then
-        setProperty('timeBarBG.visible', false)
-    end
-    if getProperty('timeBar') ~= nil then
-        setProperty('timeBar.visible', false)
-    end
-    if getProperty('timeTxt') ~= nil then
-        setProperty('timeTxt.visible', false)
-    end
 end
 function onStepHit()
     if curStep == 50 then
         for i = 0,3 do
+            setProperty('uiGroup.visible', true)
+            setProperty('timeTxt.visible', false)
+            setProperty('timeBar.visible', false)
             setPropertyFromGroup('playerStrums', i, 'alpha', 0)
             setPropertyFromGroup('playerStrums', i, 'visible', true)
             setPropertyFromGroup('opponentStrums', i, 'alpha', 0)
             setPropertyFromGroup('opponentStrums', i, 'visible', true)
+            
         end
         for i = 0, 3 do
             doTweenAlpha('playerFadeIn'..i, 'playerStrums.members['..i..']', 1, 1, 'sineInOut')
